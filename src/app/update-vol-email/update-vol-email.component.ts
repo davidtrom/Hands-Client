ea640edc84b3638dd86d4cd926b16792020b6492
David Trombello <dtrombello@gmail.com>
2020-05-04 01:04:27 -0400

    clean up edit email for volunteers component

--- src/app/update-vol-email/update-vol-email.component.ts
+++ src/app/update-vol-email/update-vol-email.component.ts
@@ -37,29 +37,12 @@ export class UpdateVolEmailComponent implements OnInit {
   get email() {
     return this.editVolEmail.get('email');
   } 
-  
-  //get form() {return this.editVolEmail.controls;}
 
   onSubmit(){
-  //   this.loginService.updateVolunteerEmail(this.volunteer$.email, this.editVolEmail.controls.newEmail.value).subscribe(data => {
-  //     this.loginService.updateCurrentVolunteer(data);
-  //     if(data === null){
-  //       this.emailTaken = true;
-  //       alert('This email address is already taken.\nPlease try again.')
-  //     }
-  //     else {
-  //       alert('Your email has been updated.')
-  //       this.editVolEmail.reset();
-  //       this.router.navigate(['/view-profile']);
-  //     }
-  //   });
-  // }
     this.loginService.checkVolunteerEmailAvailability(this.editVolEmail.controls.newEmail.value).subscribe(data=> {this.emailTaken = data;
       console.log(data);
       if(data){
-        //alert('This username/email is already taken. \nPlease try again.');
         this.emailTaken = true;
-        this.editVolEmail.reset();
       }
       else{
       this.loginService.updateVolunteerEmail(this.volunteer$.email, this.editVolEmail.controls.newEmail.value).subscribe(data =>{
@@ -67,7 +50,6 @@ export class UpdateVolEmailComponent implements OnInit {
       } );
       alert('Your username/email address have been updated.');
       this.editVolEmail.reset();
-      //location.reload();
       }
     });
   }
